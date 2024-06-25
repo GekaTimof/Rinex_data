34c0c328a418acf8d4236290e12ea79772047a5a
evgeniy <geka.timof@mail.ru>
2024-06-25 19:02:54 +0800

    add get_data_demon.py - file that we will start every day by demon, this file start other file to det rinex data, unpack it and convert to json
    rename get_data_from_zip.py to unpack_data.py
    
    finish unpack_data.py dy adding conversion from crx to rnx (use sell command CRX2RNX)

---
+++ Get_data/unpack_data.py
@@ -0,0 +1,56 @@
+import os
+import zipfile
+import gzip
+import shutil
+
+# test datas
+date = "2024-01-01"
+directory = f"../Parsed_rinex_data/{date}"
+#file_name = f"{date}.zip"
+
+# shell command CRX2RNX
+crx2rnx = "CRX2RNX"
+
+# create directory and save unpacked data in it
+def unpack_data(directory, file_name):
+    directory += "-rinex"
+    # create directory for date
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    # extracting data from zip
+    with zipfile.ZipFile(file_name, 'r') as zip_file:
+        print("Start extracting %s" % file_name)
+        zip_file.extractall(directory)
+        print("Finish extracting %s" % file_name)
+
+    #delete zip file
+    os.remove(file_name)
+
+    # unpack gz files
+    print("Start extracting rinex data from gz")
+    for file_gz in os.listdir(directory):
+        file_link = directory + "/" + file_gz
+
+        if file_gz[-3:] == ".gz":
+            with gzip.open(file_link, 'rb') as f_in:
+                with open(file_link[:-3], 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+
+        os.remove(file_link)
+    print("Finish extracting rinex data from gz")
+
+    # convert crx to rnx
+    directory += "-rinex"
+    print("Start converting crx to rnx")
+    for file_crx in os.listdir(directory):
+        file_link = directory + "/" + file_crx
+
+        if file_link[-4:] == ".crx":
+            # build shell command
+            shell_command = ''.join([crx2rnx, " ", file_link])
+            # run shell command
+            os.system(shell_command)
+
+            os.remove(file_link)
+    print("Finish converting crx to rnx")
